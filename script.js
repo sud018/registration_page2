6fe791ffb3e2aeac411954a2949711e153c06ea0
sud018 <sudheeraya18@gmail.com>
2021-06-07 11:42:51 +0530

    initial commit

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ function doctorinfo() {
   document.getElementById("doctor-info").style.display = "inline";
   document.getElementById("student-info").style.display = "none";
   document.getElementById("other-info").style.display = "none";
+  document.getElementById("pre").style.position = "relative";
+  document.getElementById("pre").style.bottom = "40px";
+  // document.classList.add("pre");
 }
 
 function studentinfo() {
   document.getElementById("student-info").style.display = "inline";
   document.getElementById("doctor-info").style.display = "none";
   document.getElementById("other-info").style.display = "none";
+  document.getElementById("pre").style.position = "relative";
+  document.getElementById("pre").style.bottom = "40px";
 }
 
 function otherinfo() {
   document.getElementById("other-info").style.display = "inline";
   document.getElementById("student-info").style.display = "none";
   document.getElementById("doctor-info").style.display = "none";
+  document.getElementById("pre").style.position = "relative";
+  document.getElementById("pre").style.bottom = "40px";
 }
 function validating(){
   document.getElementById("validate").style.display="inline";
